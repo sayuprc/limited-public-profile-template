4ec94cbc841a94f34e78f36372b5a5b5808f9c57
sayuprc <mkpoto15@gmail.com>
2024-02-14 23:52:53 +0900

    Add generate feature

--- src/app/Http/Controllers/QR/GenerateController.php
+++ src/app/Http/Controllers/QR/GenerateController.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Controllers\QR;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\QR\GenerateRequest;
+use App\UseCases\QR\Generate\GenerateInterface;
+use App\UseCases\QR\Generate\GenerateRequest as QRGenerateRequest;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class GenerateController extends Controller
 {
@@ -19,5 +23,20 @@ class GenerateController extends Controller
         $time = $now->addHour()->format('H:i');
 
         return view('qr.generate', compact('date', 'time'));
+    }
+
+    /**
+     * @param GenerateRequest   $request
+     * @param GenerateInterface $handler
+     *
+     * @return RedirectResponse
+     */
+    public function handle(GenerateRequest $request, GenerateInterface $handler): RedirectResponse
+    {
+        $expiredAt = now()->createFromFormat('Y-m-d H:i', implode(' ', $request->validated()));
+
+        $response = $handler->handle(new QRGenerateRequest($request->user()->user_id, $expiredAt));
+
+        return redirect()->route('qr.show', ['qr_code_id' => $response->qrCodeId]);
     }
 }
