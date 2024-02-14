a8e61da16716f392963009cd16fb43a2fda0f1ad
sayuprc <mkpoto15@gmail.com>
2024-02-14 22:16:40 +0900

    Migrate

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,12 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->string('user_id', 36);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+
+            $table->primary('user_id');
         });
     }
 
