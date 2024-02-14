0fb9b597fa9bd8c881647426675dfa683095ad3a
sayuprc <mkpoto15@gmail.com>
2024-02-14 23:30:30 +0900

    Add link

--- src/resources/views/layout/app.blade.php
+++ src/resources/views/layout/app.blade.php
@@ -15,6 +15,7 @@
         <header class="flex h-full">
             <div class="my-auto grow">
                 <nav class="flex justify-start">
+                    <a class="mr-8" href="{{ route('qr.generate.form') }}">QRコード作成</a>
                     <a class="mr-8" href="{{ route('logout') }}">ログアウト</a>
                 </nav>
             </div>
