198246acabc15e2b1bc27406b482aadedb9a6181
sayuprc <mkpoto15@gmail.com>
2024-02-14 22:49:35 +0900

    Add middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [LoginController::class, 'handle'])->name('login');
-Route::get('login', [LoginController::class, 'showForm'])->name('login.form');
+Route::middleware('guest')->group(function (): void {
+    Route::post('login', [LoginController::class, 'handle'])->name('login');
+    Route::get('login', [LoginController::class, 'showForm'])->name('login.form');
+});
 
-Route::get('dashboard', function (): void {
-    // TODO
-})->name('dashboard');
+Route::middleware('auth')->group(function (): void {
+    Route::get('dashboard', function (): void {
+        // TODO
+    })->name('dashboard');
+});
