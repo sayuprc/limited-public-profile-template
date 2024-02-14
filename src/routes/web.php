eaa162494fc4edec1a6def220f4521f518d5ddc0
sayuprc <mkpoto15@gmail.com>
2024-02-14 22:42:31 +0900

    Add route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', [LoginController::class, 'handle'])->name('login');
 Route::get('login', [LoginController::class, 'showForm'])->name('login.form');
