870f4ba85d347b8a8126eedb5faf3b55257ef82f
sayuprc <mkpoto15@gmail.com>
2024-02-14 22:59:19 +0900

    Update middleware

--- src/app/Http/Middleware/RedirectIfAuthenticated.php
+++ src/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -23,7 +22,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect()->route('dashboard');
             }
         }
 
