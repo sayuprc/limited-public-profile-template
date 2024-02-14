189837c85fac25b0c648939c6aa1afac9239bcb4
sayuprc <mkpoto15@gmail.com>
2024-02-14 23:55:52 +0900

    Add NopGenerateAction.php

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Debug\Actions\QR\Generate\NopGenerateAction;
+use App\UseCases\QR\Generate\GenerateInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
     public function register(): void
     {
@@ -17,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
     public function boot(): void
     {
+        $this->app->bind(GenerateInterface::class, NopGenerateAction::class);
     }
 }
