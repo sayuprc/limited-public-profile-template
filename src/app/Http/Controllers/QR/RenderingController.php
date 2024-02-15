fc61fb7eaf3ca7f2dd4a8d7a7cdcf956e6bf8a26
sayuprc <mkpoto15@gmail.com>
2024-02-15 21:54:10 +0900

    Add RenderingController.php

---
+++ src/app/Http/Controllers/QR/RenderingController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\QR;
+
+use App\Http\Controllers\Controller;
+use App\UseCases\QR\Rendering\RenderingInterface;
+use App\UseCases\QR\Rendering\RenderingRequest;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class RenderingController extends Controller
+{
+    /**
+     * @param Request            $request
+     * @param string             $qrCodeId
+     * @param RenderingInterface $handler
+     *
+     * @return View
+     */
+    public function handle(Request $request, string $qrCodeId, RenderingInterface $handler): View
+    {
+        $response = $handler->handle(new RenderingRequest($request->user()->user_id, $qrCodeId));
+
+        return view('qr.show', [
+            'expiredAt' => $response->expiredAt->format('Y-m-d H:i'),
+            'data' => $response->data,
+        ]);
+    }
+}
