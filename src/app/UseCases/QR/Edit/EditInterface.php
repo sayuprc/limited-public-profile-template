4e66ec4097015a34f672d7a502a5aece55d177d2
sayuprc <mkpoto15@gmail.com>
2024-02-15 23:31:15 +0900

    Add method

--- src/app/UseCases/QR/Edit/EditInterface.php
+++ src/app/UseCases/QR/Edit/EditInterface.php
@@ -6,4 +6,10 @@ namespace App\UseCases\QR\Edit;
 
 interface EditInterface
 {
+    /**
+     * @param EditRequest $request
+     *
+     * @return EditResponse
+     */
+    public function handle(EditRequest $request): EditResponse;
 }
